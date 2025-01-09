33b4d4ae3da224766a0ca6102d50007e76a7268d
Kade O'Crowley <kadeocrowley@gmail.com>
2025-01-09 07:07:31 -0700

    add upper

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,14 +1,16 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
+        TextInfo textInfo = CultureInfo.CurrentCulture.TextInfo;
         Console.Write("What is your first name? ");
-        string firstName = Console.ReadLine();
+        string firstName = textInfo.ToTitleCase(Console.ReadLine());
 
         Console.Write("What is your last name? ");
-        string lastName = Console.ReadLine();
+        string lastName = textInfo.ToTitleCase(Console.ReadLine());
 
         Console.Write($"\nYour name is {lastName}, {firstName} {lastName}.");
     }
