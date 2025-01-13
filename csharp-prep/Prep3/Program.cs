42db1fea5cdde1abe88cc568ada94e093d58d97b
Kade O'Crowley <kadeocrowley@gmail.com>
2025-01-13 09:03:16 -0700

    done

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,23 +5,31 @@ class Program
     static void Main(string[] args)
     {
         string play = "yes";
-        do
-        {
+        do{
             Console.WriteLine("Hello Prep3 World!");
             Random randomgenerator = new Random();
-            int magicNumber = randomgenerator.Next(1, 10);
+            int magicNumber = randomgenerator.Next(1,10);
             int guessNumber = 0;
             int guesses = 1;
-
+            
 
             Console.WriteLine("guess a number between 1 and 10.");
-            do
-            {
+            do{
                 Console.Write("what is your guess:");
                 guessNumber = int.Parse(Console.ReadLine());
-                Console.Write(guessNumber);
+                if(guessNumber > magicNumber){
+                    Console.WriteLine("Lower");
+                }else if(guessNumber < magicNumber){
+                    Console.WriteLine("Higher");
+                }else if(guessNumber == magicNumber){
+                    Console.WriteLine($"Good Job you got it in {guesses} guesses!!");
+                    Console.Write("Do you want to play again: ");
+                    play = Console.ReadLine();
+                }else{
+                    Console.WriteLine("Try again");
+                }
+                guesses = guesses += 1;
 
-            } while (guessNumber != magicNumber);
-        } while (play == "yes");
+            }while (guessNumber != magicNumber);}while (play == "yes");
     }
 }
