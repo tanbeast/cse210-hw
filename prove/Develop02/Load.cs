a30f24a1e257844a02dffb6544b78b0867ba0afc
Kade O'Crowley <kadeocrowley@gmail.com>
2025-01-30 09:56:16 -0700

    load works

--- prove/Develop02/Load.cs
+++ prove/Develop02/Load.cs
@@ -1,5 +1,19 @@
 using System.IO;
 
 public class Load{
-    
+    public string FileName(){
+        Console.Write("What is the filename? ");
+        string fileName = Console.ReadLine();
+        return fileName;  
+    }
+    public void LoadFile(string file){
+        using (StreamWriter outputFile = new StreamWriter(file, true)){
+            string filename = "myFile.txt";
+        string[] lines = System.IO.File.ReadAllLines(filename);
+
+        foreach (string line in lines){
+            outputFile.WriteLine(line); 
+        }
+        }
+    }
 }
