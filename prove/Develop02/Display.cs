e270b71eecab3a220b73c9623b588ff6d94ac9d5
Kade O'Crowley <kadeocrowley@gmail.com>
2025-01-30 09:56:37 -0700

    display works

--- prove/Develop02/Display.cs
+++ prove/Develop02/Display.cs
@@ -1,5 +1,10 @@
-using System.Reflection.Metadata.Ecma335;
-
 public class Display{
-    
+    public void Read(){
+        string filename = "myFile.txt";
+        string[] lines = System.IO.File.ReadAllLines(filename);
+
+        foreach (string line in lines){
+            Console.WriteLine(line);
+        }
+    }
 }
