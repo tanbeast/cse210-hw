cb5d652543a18b13c3352def2c3f8fdf5929d09e
Kade O'Crowley <kadeocrowley@gmail.com>
2025-02-05 01:57:35 -0700

    step 4

--- prepare/Learning03/Fractions.cs
+++ prepare/Learning03/Fractions.cs
@@ -2,4 +2,16 @@ using System;
 class Fraction{
     private int _top;
     private int _bottom;
+    public void Test(){
+        _top = 1;
+        _bottom = 1;
+    }
+    public void Test(int top){
+        _top = top;
+        _bottom = 1;
+    }
+    public void Test(int top, int bottom){
+        _top = top;
+        _bottom = bottom;
+    }
 }
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,5 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
+        Console.WriteLine("Hello Learning03 World!");
+        Fraction one = new Fraction();
+        one.Test();
+        
+        Fraction two = new Fraction();
+        two.Test(6);
+        
+        Fraction three = new Fraction();
+        three.Test(6,7);
     }
 }
