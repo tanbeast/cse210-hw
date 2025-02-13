b4582c2cf662b19d780bfce1f339e424cd6cf219
Kade O'Crowley <kadeocrowley@gmail.com>
2025-02-12 20:08:26 -0700

    reference

--- prove/Develop03/Reference.cs
+++ prove/Develop03/Reference.cs
@@ -1,1 +1,35 @@
- 
+class Reference
+    {
+        public string Book { get; private set; }
+        public int Chapter { get; private set; }
+        public int StartVerse { get; private set; }
+        public int? EndVerse { get; private set; }  // Nullable for single verses
+
+        public Reference(string book, int chapter, int verse)
+        {
+            Book = book;
+            Chapter = chapter;
+            StartVerse = verse;
+            EndVerse = null;
+        }
+        
+        public Reference(string book, int chapter, int startVerse, int endVerse)
+        {
+            Book = book;
+            Chapter = chapter;
+            StartVerse = startVerse;
+            EndVerse = endVerse;
+        }
+
+        public override string ToString()
+        {
+            if (EndVerse.HasValue)
+            {
+                return $"{Book} {Chapter}:{StartVerse}-{EndVerse}";
+            }
+            else
+            {
+                return $"{Book} {Chapter}:{StartVerse}";
+            }
+        }
+    }
