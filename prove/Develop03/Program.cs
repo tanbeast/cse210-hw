3468c2344c49d6fb2a57aef33500a461f0b54b0a
Kade O'Crowley <kadeocrowley@gmail.com>
2025-02-12 20:14:21 -0700

    program with bug

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,32 @@
 using System;
 
-class Program
-{
+class Program{
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        Reference reference = new Reference("Proverbs", 3, 5, 6);
+        string scriptureText = "Trust in the Lord with all thine heart and lean not unto thine own understanding.";
+        Scripture scripture = new Scripture(reference, scriptureText);
+
+        Console.Clear()
+        scripture.Display();
+
+        while (true)
+        {
+            Console.WriteLine("\nPress Enter to hide more words or type 'quit' to exit:");
+            string userInput = Console.ReadLine().Trim().ToLower();
+
+            if (userInput == "quit"){
+                break;
+            }
+            scripture.HideRandomWords();
+            Console.Clear();
+            scripture.Display();
+
+            if (scripture.AllWordsHidden())
+            {
+                Console.WriteLine("\nAll words are hidden! You've completed the scripture memorization.");
+                break;  
+            }
+        }
     }
 }
