a6e26c969189beca84a4cba655d4d263a2497a42
Kade O'Crowley <kadeocrowley@gmail.com>
2025-02-13 07:05:47 -0700

    first work on extra

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,21 +1,43 @@
 using System;
+using System.ComponentModel;
+using System.Linq.Expressions;
+using Microsoft.Win32.SafeHandles;
 
 class Program{
     static void Main(string[] args)
-    {
-        Reference reference = new Reference("Proverbs", 3, 5, 6);
-        string scriptureText = "Trust in the Lord with all thine heart and lean not unto thine own understanding.";
-        Scripture scripture = new Scripture(reference, scriptureText);
+    {   
+        while(true){
+        Console.Write("Please enter the scripture to memorize");
+        var thing = Console.ReadLine();
+        switch(thing){
+            case "1":
+                Reference reference = new Reference("Proverbs", 3, 5, 6);
+                string scriptureText = "Trust in the Lord with all thine heart and lean not unto thine own understanding.";
+                Scripture scripture = new Scripture(reference, scriptureText);
+                break;
+            case "2":
+                Reference reference = new Reference("Proverbs", 6, 5, 6);
+                string scriptureText = "Trust in the Lord with all thine heart and lean not unto thine own understanding.";
+                Scripture scripture = new Scripture(reference, scriptureText);
+                break;
+            case "3":
+                return;
+
+        }
+
+
+        
 
         Console.Clear();
+        Console . WriteLine("your chosen scripture:");
         scripture.Display();
 
         while (true)
         {
-            Console.WriteLine("\nPress Enter to hide more words or type 'quit' to exit:");
+            Console.WriteLine("\nPress Enter to hide more words or type 'back' to return to menu:");
             string userInput = Console.ReadLine().Trim().ToLower();
 
-            if (userInput == "quit"){
+            if (userInput == "back"){
                 break;
             }
             scripture.HideRandomWords();
@@ -27,6 +49,6 @@ class Program{
                 Console.WriteLine("\nAll words are hidden! You've completed the scripture memorization.");
                 break;  
             }
-        }
+        }}
     }
 }
