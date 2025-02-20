f7ff2e1ad27d6205ad0be43ee13a921d294e7298
Kade O'Crowley <kadeocrowley@gmail.com>
2025-02-20 05:45:01 -0700

    main progrm done

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,16 @@
 using System;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Learning04 World!");
+class Program{
+    static void Main(string[] args){
+        Assignment one = new Assignment("David Laed", "Multiplication");
+        Console.WriteLine(one.GetSummary());
+
+        Math two = new Math("Kristy Wood", "Fractions", "7.5", "3-21");
+        Console.WriteLine(two.GetSummary());
+        Console.WriteLine(two.GetHomeworkList());
+
+        Writing three = new Writing("Mary Mack", "History", "The Causes of World War");
+        Console.WriteLine(three.GetSummary());
+        Console.WriteLine(three.GetWritingInformation());
     }
 }
