8837a10aebb6b437f38752dc67eb15fc74f789c3
Kade O'Crowley <kadeocrowley@gmail.com>
2025-02-20 05:42:21 -0700

    Math done

---
+++ prepare/Learning04/Math.cs
@@ -0,0 +1,13 @@
+public class Math : Assignment{
+    private string _textbookSection;
+    private string _problems;
+
+    public Math(string studentName, string topic, string textbookSection, string problems)
+        : base(studentName, topic){
+        _textbookSection = textbookSection;
+        _problems = problems;
+    }
+    public string GetHomeworkList(){
+        return $"Section {_textbookSection} Problems {_problems}";
+    }
+}
