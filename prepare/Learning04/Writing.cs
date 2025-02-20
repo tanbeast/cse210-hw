b31ee1da78e4a461fe89d90b34b9e5eb7bfb3b02
Kade O'Crowley <kadeocrowley@gmail.com>
2025-02-20 05:42:02 -0700

    writing done

---
+++ prepare/Learning04/Writing.cs
@@ -0,0 +1,12 @@
+public class Writing : Assignment{
+    private string _title;
+    public Writing(string studentName, string topic, string title)
+        : base(studentName, topic){
+        _title = title;
+    }
+    public string GetWritingInformation(){
+        string studentName = GetStudentName();
+
+        return $"{_title} by {studentName}";
+    }
+}
