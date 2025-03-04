539e04a90dd7fcf6be389fc1a947af5686b60f63
Kade O'Crowley <kadeocrowley@gmail.com>
2025-03-04 06:00:49 -0700

    square works

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -5,5 +5,8 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning05 World!");
+        var square1 = new Square("Red", 5);
+        Console.WriteLine($"Shape Color: {square1.Color}, Area: {square1.GetArea()}");
+
     }
 }
---
+++ prepare/Learning05/Shape.cs
@@ -0,0 +1,10 @@
+public abstract class Shape{
+    public string Color { get; set; }
+
+    public Shape(string color){
+        Color = color;
+    }
+
+    public abstract double GetArea();
+}
+ 
