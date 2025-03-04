07d2689a3ab6578f0abef40a26bf63a2f4423d30
Kade O'Crowley <kadeocrowley@gmail.com>
2025-03-04 06:06:53 -0700

    other two

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -5,8 +5,12 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning05 World!");
-        var square1 = new Square("Red", 5);
+        var square1 = new Square("Red", 7);
+        var rect = new Rectangle("Blue", 4, 5);
+        var circ = new Circle("Black", 5);
         Console.WriteLine($"Shape Color: {square1.Color}, Area: {square1.GetArea()}");
+        Console.WriteLine($"Shape Color: {rect.Color}, Area: {rect.GetArea()}");
+        Console.WriteLine($"Shape Color: {circ.Color}, Area: {circ.GetArea()}");
 
     }
 }
